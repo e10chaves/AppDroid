b60927d475c01c44a13b04c88e77e91982faca9a
e <LEUEMANUEL>
2019-04-24 14:42:22 -0400

    import

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, Request, response, redirect
+
+
 app = Flask(__name__)
+
+
 @app.route(‘/’)
 def main():
- return redirect(‘static/index.html’)
+    return redirect(‘static/index.html’)
